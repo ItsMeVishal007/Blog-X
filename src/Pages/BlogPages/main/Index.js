eb5b61aa7ca17ff73a558c57bf3f579c26c5812d
Yash <yash.codedrill@gmail.com>
2021-04-23 11:48:26 +0530

    changes

--- src/Pages/BlogPages/main/Index.js
+++ src/Pages/BlogPages/main/Index.js
@@ -1,35 +1,43 @@
-import React,{useState,useEffect} from 'react'
-import { Link } from 'react-router-dom';
-import NewsCard from '../../../component/NewsCard/NewsCard.component';
-import './NewsCardsContainer.css';
+// import React,{useState,useEffect} from 'react'
+// import { Link } from 'react-router-dom';
+// import NewsCard from '../../../component/NewsCard/NewsCard.component';
+// import './NewsCardsContainer.css';
 
-const BlogPages = (props) => {
-  const [newsTitle,setNewsTitle] = useState()
-  const[newsImg,setNewsImg] = useState()
-  useEffect(() => {
-    const fetchData = async() =>{
-  const urlData =await fetch('http://newsapi.org/v2/top-headlines?q=ai&apiKey=1905ac9542f24645868281f04e1538b6')
-  const result = await urlData.json()
-  // console.log(result.articles)
-  setNewsTitle(result.articles[1].title)
-  setNewsImg(result.articles.urlToImage)
-    }
+// const BlogPages = (props) => {
+//   const [news,setNews] = useState()
+
+//   useEffect(() => {
+//     const fetchData = async() =>{
+//   const urlData =await fetch('http://newsapi.org/v2/top-headlines?q=ai&apiKey=1905ac9542f24645868281f04e1538b6')
+//   const result = await urlData.json()
+//   // console.log(result.articles)
+//   setNews(result.articles)
+
+//     }
   
-     fetchData()
-   }, [])
-  console.log(newsTitle,newsImg)
-  console.log(props)
-  return (
-    <div className='NewsCardsContainer'>
+//      fetchData()
+//    }, [])
+//   console.log(news)
+ 
+//   return (
+//     <div className='NewsCardsContainer'>
 
+// {news?
+//   news.map((data)=>
+//   (
+//     <NewsCard 
+//     image={data.urlToImage}
+//     Title={data.title}
+        
+//         />
+//   ))
+//   :
+//         'Loading...'
+  
+     
+// }
+//     </div>
+//   )
+// }
 
-      <NewsCard 
-
-    Title = {newsTitle}
-    />
-      
-    </div>
-  )
-}
-
-export default BlogPages;
+// export default BlogPages;
